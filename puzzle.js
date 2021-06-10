0109e840e3ad45fe57f1bf4791e4fb27384087f3
AYA-kurokiri09 <ice6e63@gmail.com>
2021-06-10 23:13:25 +0900

    四角形とテキストの描画処理を追加

--- puzzle.js
+++ puzzle.js
@@ -29,7 +29,7 @@
 	const layerCtrl = function(canvas, zindex){
 		const context = canvas.getContext("2d");//canvasオブジェクトが返される。参考: https://qiita.com/manten120/items/86c087b937708697acec
 		this.getCanvas = () => canvas;
-		this.getContent = () => context;
+		this.getContext = () => context;
 
 		let beforeIndex = zindex;
 
@@ -42,4 +42,63 @@
 		this.resetIndex = () => canvas.style.zIndex = beforeIndex;
 	};
 
+	//プロトタイプによるメモリ節約
+	//参考: https://www.sejuku.net/blog/47722
+	
+	layerCtrl.prototype = {
+		/**
+		 * キャンバスサイズをセット
+		 * @param w
+		 * @param h
+		 */
+		//オブジェクト内部での短縮メソッド定義: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Functions/Method_definitions
+		setSize(w,h) {
+			const canvas = this.getCanvas();
+			canvas.setAttribute("width", w);
+			canvas.setAttribute("height", h);
+		},
+
+		/**
+		 * 四角形の描画
+		 * @param rect [左座標, 上座標, 幅, 高さ]
+		 * @param fillColor 塗りつぶし色 null ... 塗りつぶしなし
+		 * @param strokeColor 線色 null ...線なし
+		 */
+		rect(rect, fillColor, strokeColor = null) {
+			const context = this.getContext();
+			context.save();
+			if (fillColor !== null) {
+				context.fillStyle = fillColor;
+				context.fillRect(...rect);//[左座標, 上座標, 幅, 高さ]を配列扱いで代入
+			}
+			if(strokeColor !== null) {
+				context.strokeColor = strokeColor;
+				context.strokeRect(...rect);
+			}
+			context.restore();
+			return this;
+		},
+
+		/**
+		 * テキストの描画
+		 * @param style テキスト属性のオブジェクト
+		 * @param textString 描画する文字列
+		 * @param x 描画位置
+		 * @param y 描画位置
+		 */
+		//Object.keys()参考: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+		//.foreach(アロー関数)参考: https://www.javadrive.jp/javascript/array/index10.html
+		text(style, textString, x, y) {
+			const context = this.getContext();
+			context.save();
+			//eはelementのe https://www.javadrive.jp/javascript/array/index10.html
+			Object.keys(style).forEach(e => context[e] = style[e]);
+			context.storkeText(textString, x, y);
+			context.fillText(textString, x, y);
+			context.restore();
+			return this;
+		}
+
+
+	}
 })();
