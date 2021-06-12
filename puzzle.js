975556b7f84ea5cf01f43bff7c5219d6f6f0c2ab
AYA-kurokiri09 <ice6e63@gmail.com>
2021-06-12 22:31:40 +0900

    キャンバス複製処理を作成

--- puzzle.js
+++ puzzle.js
@@ -9,6 +9,8 @@
 //中層: ピースレイヤ
 //最上層: アニメレイヤ
 
+//Canvasのキャンバスサイズと表示サイズの違いについて: http://note.affi-sapo-sv.com/js-canvas-size.php
+
 ( () => {
 
 	//レイヤのサイズ設定 (オブジェクト)
@@ -47,107 +49,153 @@
 	//プロトタイプによるメモリ節約
 	//参考: https://www.sejuku.net/blog/47722
 	
-	layerCtrl.prototype = {
-		/**
-		 * キャンバスサイズをセット
-		 * @param w
-		 * @param h
-		 */
-		//オブジェクト内部での短縮メソッド定義: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Functions/Method_definitions
-		setSize(w,h) {
-			const canvas = this.getCanvas();
-			canvas.setAttribute("width", w);
-			canvas.setAttribute("height", h);
-		},
+		layerCtrl.prototype = {
 
-		/**
-		 * 四角形の描画
-		 * @param rect [左座標, 上座標, 幅, 高さ]
-		 * @param fillColor 塗りつぶし色 null ... 塗りつぶしなし
-		 * @param strokeColor 線色 null ...線なし
-		 */
-		rect(rect, fillColor, strokeColor = null) {
-			const context = this.getContext();
-			context.save();
-			if (fillColor !== null) {
-				context.fillStyle = fillColor;
-				context.fillRect(...rect);//[左座標, 上座標, 幅, 高さ]を配列扱いで代入
+			/**
+         	* キャンバスにスタイルをセット
+         	* @param styleObj 例：{ widt: "100px" , height : "100px" }
+         	*/
+			setStyle(styleObj) {
+				const canvas = this.getCanvas;
+				Object.keys(styleObj).forEach(e => canvas.style[e] = styleObj[e]);
+			},
+
+			/**
+			 * キャンバスサイズをセット
+			 * @param w
+			 * @param h
+			 */
+			//オブジェクト内部での短縮メソッド定義: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Functions/Method_definitions
+			setSize(w,h) {
+				const canvas = this.getCanvas();
+				canvas.setAttribute("width", w);
+				canvas.setAttribute("height", h);
+			},
+
+			/**
+			 * 四角形の描画
+			 * @param rect [左座標, 上座標, 幅, 高さ]
+			 * @param fillColor 塗りつぶし色 null ... 塗りつぶしなし
+			 * @param strokeColor 線色 null ...線なし
+			 */
+			rect(rect, fillColor, strokeColor = null) {
+				const context = this.getContext();
+				context.save();
+				if (fillColor !== null) {
+					context.fillStyle = fillColor;
+					context.fillRect(...rect);//[左座標, 上座標, 幅, 高さ]を配列扱いで代入
+				}
+				if(strokeColor !== null) {
+					context.strokeColor = strokeColor;
+					context.strokeRect(...rect);
+				}
+				context.restore();
+				return this;
+			},
+
+			/**
+			 * テキストの描画
+			 * @param style テキスト属性のオブジェクト
+			 * @param textString 描画する文字列
+			 * @param x 描画位置
+			 * @param y 描画位置
+			 */
+			//Object.keys()参考: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+			//.foreach(アロー関数)参考: https://www.javadrive.jp/javascript/array/index10.html
+			text(style, textString, x, y) {
+				const context = this.getContext();
+				context.save();
+				//eはelementのe https://www.javadrive.jp/javascript/array/index10.html
+				Object.keys(style).forEach(e => context[e] = style[e]);
+				context.storkeText(textString, x, y);//ふちどり
+				context.fillText(textString, x, y);//塗りつぶし
+				context.restore();
+				return this;
+			},
+
+			/**
+			 * イメージデータの取得
+			 * @param rect
+			 * @returns {ImageData}
+			 */
+			getImageData(rect) {
+				return this.getContext().getImageData(...rect);
+			},
+
+			/**
+			 * イメージの描画
+			 * @param image
+			 * @param x
+			 * @param y
+			 * @returns {layerCtrl}
+			 */
+			putImageData(image, x, y) {
+				this.getContext().putImageData(image, x, y);
+				return this;
+			},
+
+			/**
+			 * キャンバス全面クリア
+			 */
+			clear() {
+				const cvs = this.getContext().clearRect(...rect);
+				this.getContext().clearRect(0, 0, cvs.width, cvs.height);
+			},
+
+			/**
+			 * キャンバスクリア
+			 * @param rect [ 左座標 , 上座標 , 幅 , 高さ ]
+			 * @returns {layerCtrl}
+			 */
+			clearRect(rect) {
+				this.getContext.clearRect(...rect);
+				return this;
+			},
+
+			/**
+			 * キャンバスのサイズと表示サイズの比率
+			 * @returns {number}
+			 */
+			getScale() {
+				const canvas = this.getCanvas();
+				return canvas.clientWidth / canvas.width;
 			}
-			if(strokeColor !== null) {
-				context.strokeColor = strokeColor;
-				context.strokeRect(...rect);
-			}
-			context.restore();
-			return this;
-		},
 
-		/**
-		 * テキストの描画
-		 * @param style テキスト属性のオブジェクト
-		 * @param textString 描画する文字列
-		 * @param x 描画位置
-		 * @param y 描画位置
-		 */
-		//Object.keys()参考: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-		//.foreach(アロー関数)参考: https://www.javadrive.jp/javascript/array/index10.html
-		text(style, textString, x, y) {
-			const context = this.getContext();
-			context.save();
-			//eはelementのe https://www.javadrive.jp/javascript/array/index10.html
-			Object.keys(style).forEach(e => context[e] = style[e]);
-			context.storkeText(textString, x, y);
-			context.fillText(textString, x, y);
-			context.restore();
-			return this;
-		},
-
-		/**
-         * イメージデータの取得
-         * @param rect
-         * @returns {ImageData}
-         */
-		getImageData(rect) {
-			return this.getContext().getImageData(...rect);
-		},
-
-		/**
-         * イメージの描画
-         * @param image
-         * @param x
-         * @param y
-         * @returns {layerCtrl}
-         */
-		putImageData(image, x, y) {
-			this.getContext().putImageData(image, x, y);
-			return this;
-		},
-
-		/**
-         * キャンバス全面クリア
-         */
-		clear() {
-			const cvs = this.getContext().clearRect(...rect);
-			this.getContext().clearRect(0, 0, cvs.width, cvs.height);
-		},
-
-		 /**
-         * キャンバスクリア
-         * @param rect [ 左座標 , 上座標 , 幅 , 高さ ]
-         * @returns {layerCtrl}
-         */
-		clearRect(rect) {
-			this.getContext.clearRect(...rect);
-			return this;
-		},
-
-		/**
-         * キャンバスのサイズと表示サイズの比率
-         * @returns {number}
-         */
-		getScale() {
-			const canvas = this.getCanvas();
-			return canvas.clientWidth / canvas.width;
 		}
 
+	/**
+	 * キャンバスを作成して配置
+	 * @param divId キャンバスを作成するdiv要素のID
+	 * @returns {[canvas, canvas]} [背景, パズル]のキャンバス配列
+	 */
+	const makeSlidePuzzle =function (divId) {
+
+		//div要素取得
+		//DOM操作: https://developer.mozilla.org/ja/docs/Learn/JavaScript/Client-side_web_APIs/Manipulating_documents
+		const parentDiv = document.getElementById(divId);
+		if (parentDiv == null) throw new Error("id:" + divId + "が見つかりません");
+
+		//キャンバスの表示サイズ
+		const viewSize = Math.min(parentDiv.clientWidth, parentDiv.clientHeight);
+
+		//キャンバスのスタイル設定
+		const style = {
+			width: viewSize + "px", height: viewSize + "px",
+			top: ((parentDiv.clientHeight - viewSize) / 2).toString() + "px",
+			left : ((parentDiv.clientWidth - viewSize) / 2).toString() + "px"
+		};
+
+		//キャンバスを3枚作成
+		//Array.prototype.map()で与えられた関数を配列のすべての要素に対して呼び出し、その結果からなる新しい配列を生成する
+		return [1,2,3].map(zindex => {
+			const cv = document.createElement("canvas");
+			const layer = new layerCtrl(cv, zindex);
+			layer.setSize(puzzleScreenInfo.size, puzzleScreenInfo.size);//setsize(w,h)
+			layer.setStyle(style);
+
+			parentDiv.appendChild(cv);//parentDivの子要素キャンバスを生成
+			return layer;
+		});
 	}
+
 })();
